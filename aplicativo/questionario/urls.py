522a57cb63f7e296a14494ddb1ca42617974a8bb
romulodf-cesar <romulodf.cesar@gmail.com>
2026-03-04 20:34:58 -0300

    concluir aplicativo front

--- aplicativo/questionario/urls.py
+++ aplicativo/questionario/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('questionario/', views.abrir_questionario, name='questionario'),
+    path('detalhequestionario/', views.detalhe_questionario, name='detalhe_questionario'),
 ]
--- aplicativo/questionario/views.py
+++ aplicativo/questionario/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 # função para renderizar o questionário
 def abrir_questionario(request):
     return render(request,'questionario/index.html')
+
+def detalhe_questionario(request):
+    return render(request,'questionario/detalhe_questionario.html')
+
