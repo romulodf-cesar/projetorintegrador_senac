8006f160f198423c17b29a5c8a9888182130030c
romulodf-cesar <romulodf.cesar@gmail.com>
2026-03-05 21:31:29 -0300

    encerrando ORM basico

--- aplicativo/questionario/models.py
+++ aplicativo/questionario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Classe é um conjunto de objetos
+# Objeto é uma instância da classe
+# Atributos são as características do objeto
+# Métodos ações do objeto
+# Toda classe começa com letra maiúscula
+# classe Lucio(Antonio) - Herança, Lucio é uma classe filha de Antonio, Antonio é a classe pai
+
+class Questionario(models.Model):
+    descricao  = models.CharField(max_length=60,null=False, blank=False)
+    def __str__(self):
+        return f"Questionário [descricao={self.descricao}]"
